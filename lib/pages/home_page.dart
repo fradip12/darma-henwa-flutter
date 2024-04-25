124f6d353cf58bfebb999478bd1c1dc3e25230cf
fradip <fradipta.alqaiyum@koltiva.com>
2024-04-25 14:48:24 +0700

    session 4 : bottom navbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,8 +3,59 @@ import 'package:apps_satu/pages/detail_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Widget> screen = [
+    Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                // Routing to another pages with simple way
+                // Navigator.of(context).push(
+                //   MaterialPageRoute(
+                //     builder: (context) => const DetailPage(),
+                //   ),
+                // );
+
+                // Routing with route named
+                // Navigator.of(context).pushNamed(
+                //   '/detail',
+                //   arguments: DetailArguments(
+                //     text: 'From Homepage',
+                //     isActive: true,
+                //   ),
+                // );
+              },
+              child: Text('Go to second screen'),
+            ),
+          ],
+        ),
+      ),
+    ),
+
+    Container(
+      child: Text('Chat Page'),
+    ), // ChatPage
+    Container(
+      child: Text('Setting Page'),
+    ), // Setting Page
+    // Profile Page
+    // ChatPage(),
+    // SettingPage(),
+    // ProfilePage(),
+  ];
+
+  var activeIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -17,37 +68,14 @@ class HomePage extends StatelessWidget {
           Icon(Icons.notifications),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  // Routing to another pages with simple way
-                  // Navigator.of(context).push(
-                  //   MaterialPageRoute(
-                  //     builder: (context) => const DetailPage(),
-                  //   ),
-                  // );
-
-                  // Routing with route named
-                  Navigator.of(context).pushNamed(
-                    '/detail',
-                    arguments: DetailArguments(
-                      text: 'From Homepage',
-                      isActive: true,
-                    ),
-                  );
-                },
-                child: Text('Go to second screen'),
-              ),
-            ],
-          ),
-        ),
-      ),
+      body: screen[activeIndex],
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: activeIndex,
+        onTap: (value) {
+          setState(() {
+            activeIndex = value;
+          });
+        },
         items: [
           BottomNavigationBarItem(
             label: 'Homepage',
